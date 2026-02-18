0e2e1f00f9226ed6ea69c9e2505dbf11423c24e3
Yoav Winstein <yoav.winstein@gmail.com>
2026-02-18 20:57:08 +0200

    make webhooks optional

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -11,62 +11,36 @@ const {
   WEBHOOK_ID_STARTUP,
   WEBHOOK_TOKEN_STARTUP,
 } = process.env;
-if (
-  !(
-    WEBHOOK_ID_EX
-    && WEBHOOK_TOKEN_EX
-    && WEBHOOK_ID_JOIN
-    && WEBHOOK_TOKEN_JOIN
-    && WEBHOOK_ID_BUG
-    && WEBHOOK_TOKEN_BUG
-    && WEBHOOK_ID_STARTUP
-    && WEBHOOK_TOKEN_STARTUP
-  )
-) {
-  throw new Error('Discord Webhook information is missing.');
-}
 
-const exceptionsWebhook = new Discord.WebhookClient({
-  id: WEBHOOK_ID_EX,
-  token: WEBHOOK_TOKEN_EX,
-});
-const joinsWebhook = new Discord.WebhookClient({
-  id: WEBHOOK_ID_JOIN,
-  token: WEBHOOK_TOKEN_JOIN,
-});
-const bugreportWebhook = new Discord.WebhookClient({
-  id: WEBHOOK_ID_BUG,
-  token: WEBHOOK_TOKEN_BUG,
-});
-const startupWebhook = new Discord.WebhookClient({
-  id: WEBHOOK_ID_STARTUP,
-  token: WEBHOOK_TOKEN_STARTUP,
-});
+const exceptionsWebhook = WEBHOOK_ID_EX && WEBHOOK_TOKEN_EX
+  ? new Discord.WebhookClient({ id: WEBHOOK_ID_EX, token: WEBHOOK_TOKEN_EX })
+  : null;
+const joinsWebhook = WEBHOOK_ID_JOIN && WEBHOOK_TOKEN_JOIN
+  ? new Discord.WebhookClient({ id: WEBHOOK_ID_JOIN, token: WEBHOOK_TOKEN_JOIN })
+  : null;
+const bugreportWebhook = WEBHOOK_ID_BUG && WEBHOOK_TOKEN_BUG
+  ? new Discord.WebhookClient({ id: WEBHOOK_ID_BUG, token: WEBHOOK_TOKEN_BUG })
+  : null;
+const startupWebhook = WEBHOOK_ID_STARTUP && WEBHOOK_TOKEN_STARTUP
+  ? new Discord.WebhookClient({ id: WEBHOOK_ID_STARTUP, token: WEBHOOK_TOKEN_STARTUP })
+  : null;
 
 export async function sendException(value: string, shardId?: number) {
-  return exceptionsWebhook
-    .send(`Shard ${shardId}: ${value}`)
-    .catch(doNothingOnError);
+  return exceptionsWebhook?.send(`Shard ${shardId}: ${value}`).catch(doNothingOnError);
 }
 export async function sendJoinEvent(value: string, shardId?: number) {
-  return joinsWebhook
-    .send(`Shard ${shardId}: ${value}`)
-    .catch(doNothingOnError);
+  return joinsWebhook?.send(`Shard ${shardId}: ${value}`).catch(doNothingOnError);
 }
 export async function sendBugreport(value: string, shardId?: number) {
-  return bugreportWebhook
-    .send(`Shard ${shardId}: ${value}`)
-    .catch(doNothingOnError);
+  return bugreportWebhook?.send(`Shard ${shardId}: ${value}`).catch(doNothingOnError);
 }
 export async function sendStartupEvent(
   shardId: number,
   justStartingUp = false,
 ) {
-  return startupWebhook
-    .send(
-      justStartingUp
-        ? `Shard ${shardId} is starting...!`
-        : `Shard ${shardId} is up!`,
-    )
-    .catch(doNothingOnError);
+  return startupWebhook?.send(
+    justStartingUp
+      ? `Shard ${shardId} is starting...!`
+      : `Shard ${shardId} is up!`,
+  ).catch(doNothingOnError);
 }
